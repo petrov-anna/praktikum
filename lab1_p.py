198bb492a1fc8b14f1a78ad27801dc0cf44f09c4
petrov-anna <anna.petrov-petrov@yandex.ru>
2021-02-18 12:04:52 +0300

    Added the age of the user

--- lab1_p.py
+++ lab1_p.py
@@ -1,2 +1,7 @@
 name = input("Enter your name, please\n")
 print("Hello, " + name)
+age = int(input("Enter your age, please\n"))
+if age < 18:
+    print("This user is minor, because his/her age is " + str(age))
+else:
+    print("This user is adult, because his/her age is " + str(age))
