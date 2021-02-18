6d883495b167d6b839c8a2b41e82a74a81799bc8
petrov-anna <anna.petrov-petrov@yandex.ru>
2021-02-18 12:30:09 +0300

    The quadratic equation

--- lab1_p.py
+++ lab1_p.py
@@ -5,3 +5,11 @@ if age < 18:
     print("This user is minor, because his/her age is " + str(age))
 else:
     print("This user is adult, because his/her age is " + str(age))
+
+# calculation
+a = 10
+b = 3
+c = 4
+print(str(a) + 'x' + '+' + str(b) + 'y' + '+' + str(c) + '=' + '0')
+D = b*b - 4*a*c
+print('Discriminant -> ' + str(D))
